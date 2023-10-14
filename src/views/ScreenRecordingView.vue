09abd429fca6682ff534cc2e7d7b23ce063fcaf9
Krle2 <djordje.krstic97@gmail.com>
2023-10-14 19:09:56 +0200

    add screen recording for desktop

--- src/views/ScreenRecordingView.vue
+++ src/views/ScreenRecordingView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <ScreenRecording class="none" />
+    <div class="desktop-version mt-15">
+      <p>
+        You need to switch to desktop, this feature is not available on the
+        mobile version.
+      </p>
+      <img
+        class="img-fluid"
+        src="https://cdni.iconscout.com/illustration/premium/thumb/sorry-we-are-closed-4004036-3310009.png"
+      />
+    </div>
   </div>
 </template>
 <script>
