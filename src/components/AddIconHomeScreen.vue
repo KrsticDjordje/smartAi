45e79262ea3cc8ff396a85e836aff42359627264
Krle2 <djordje.krstic97@gmail.com>
2023-11-26 13:14:49 +0100

    home screen test

--- src/components/AddIconHomeScreen.vue
+++ src/components/AddIconHomeScreen.vue
@@ -13,6 +13,18 @@ export default {
       if ("installPrompt" in window) {
         // Ako je prompt dostupan, pozovite ga
         window.installPrompt.prompt();
+
+        // Sačekajte da se korisnik odluči pre nego što se sakrije prompt
+        window.installPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("Korisnik je prihvatio dodavanje na početni ekran");
+          } else {
+            console.log("Korisnik je odbio dodavanje na početni ekran");
+          }
+
+          // Postavite instalacijski prompt na null da biste mogli ponovo zatražiti instalaciju
+          window.installPrompt = null;
+        });
       }
     },
   },
