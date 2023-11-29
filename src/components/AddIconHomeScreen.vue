0cf6508c20759fa7ae8f8dfb39ef9b860011f839
Krle2 <djordje.krstic97@gmail.com>
2023-11-29 20:53:30 +0100

    Home screen back old logic

--- src/components/AddIconHomeScreen.vue
+++ src/components/AddIconHomeScreen.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <!-- Dugme za Android -->
-    <button v-if="!isAppInstalled && !isIOS()" @click="addToHomeScreen">
-      Install on your phone (Android)
+    <!-- Vaša Vue komponenta i sadržaj -->
+    <button v-if="!isAppInstalled" @click="addToHomeScreen">
+      Install on your phone
     </button>
-
-    <!-- Dugme za iOS -->
-    <button v-if="!isAppInstalled && isIOS()" @click="showIOSInstructions">
-      Install on your phone (iOS)
-    </button>
-
-    <!-- Poruka kada je aplikacija instalirana (samo za Android) -->
-    <div v-if="isAppInstalled && !isIOS()">Installed</div>
+    <div v-else>Installed</div>
   </div>
 </template>
-
-<script>
+  
+  <script>
 export default {
   data() {
     return {
@@ -29,39 +22,48 @@ export default {
       return /iphone|ipad|ipod/.test(userAgent);
     },
     checkAppInstalled() {
-      // Posebna logika za Android i iOS
-      if (this.isIOS()) {
-        // iOS ne podržava beforeinstallprompt, pa se oslanjamo na standalone mod
-        this.isAppInstalled = window.matchMedia(
-          "(display-mode: standalone)"
-        ).matches;
-      } else {
-        // Za Android, proveravamo installPrompt
-        this.isAppInstalled = !!this.installPrompt;
+      // Provera da li je aplikacija već instalirana
+      if (window.matchMedia("(display-mode: standalone)").matches) {
+        this.isAppInstalled = true;
       }
     },
     addToHomeScreen() {
       if (this.installPrompt) {
+        // Ako je prompt dostupan, pozovite ga
         this.installPrompt.prompt();
+
+        // Sačekajte da se korisnik odluči
         this.installPrompt.userChoice.then((choiceResult) => {
-          this.isAppInstalled = choiceResult.outcome === "accepted";
+          if (choiceResult.outcome === "accepted") {
+            console.log("Korisnik je prihvatio dodavanje na početni ekran");
+            this.isAppInstalled = true; // Postavite da je aplikacija instalirana
+          } else {
+            console.log("Korisnik je odbio dodavanje na početni ekran");
+          }
+
+          // Resetujte instalacijski prompt
           this.installPrompt = null;
         });
       }
     },
     showIOSInstructions() {
       alert(
-        "Da dodate našu aplikaciju na početni ekran: otvorite Safari meni za deljenje i izaberite 'Add to Home Screen'."
+        "To add our app to your home screen: open Safari's sharing menu and select 'Add to Home Screen'."
       );
     },
   },
   mounted() {
     window.addEventListener("beforeinstallprompt", (e) => {
-      e.preventDefault();
-      this.installPrompt = e;
+      e.preventDefault(); // Sprečite odmah prikazivanje prompta
+      this.installPrompt = e; // Sačuvajte event za kasnije
     });
 
-    this.checkAppInstalled();
+    if (this.isIOS()) {
+      this.showIOSInstructions(); // Prikazuje obaveštenje za iOS korisnike
+    }
+
+    this.checkAppInstalled(); // Proverava da li je aplikacija već instalirana
   },
 };
 </script>
+  
